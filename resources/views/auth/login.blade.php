d6769123bbc9a444479838ae2df812db84821c4e
Ashik Chowdhury <ashikbracu@gmail.com>
2018-09-06 00:22:25 +0600

    login page added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,80 @@
-@extends('layouts.app')
+<!DOCTYPE HTML>
+<html lang="zxx">
+<head>
+    <title>Features Bangla</title>
+    <!-- Meta-Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Particles Login Form Form a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- //Meta-Tags -->
+    <!-- Stylesheets -->
+    <link href="/css/main/login_style.css" rel='stylesheet' type='text/css' />
+    <!-- online fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Amiri:400,400i,700,700i" rel="stylesheet">
+</head>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+<body>
+<!--  particles  -->
+<div id="particles-js"></div>
+<!-- //particles -->
+<div class="w3ls-pos">
+    <h2>Features Bangla</h2>
+    <div class="w3ls-login box">
+        <!-- form starts here -->
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+            <div class="agile-field-txt{{ $errors->has('email') ? ' has-error' : '' }}">
+                <div class="col-md-6">
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" required placeholder="info@example.com" autofocus>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
-        </div>
+
+            <div class="agile-field-txt{{ $errors->has('password') ? ' has-error' : '' }}">
+                <input type="password" name="password" placeholder="******" required="" id="password" />
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                     <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+                <div class="agile_label">
+                    <input class="check" for="check3" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                </div>
+            </div>
+
+            <div class="w3ls-bot">
+                <input type="submit" value="LOGIN">
+                <a class="forget" href="{{ route('password.request') }}">
+                    Forgot Your Password?
+                </a>
+            </div>
+        </form>
     </div>
+    <!-- //form ends here -->
+    <!--copyright-->
+    <div class="copy-wthree">
+        <p>© 2018 Features Bangla. All Rights Reserved</p>
+    </div>
+    <!--//copyright-->
 </div>
-@endsection
+<!-- scripts required  for particle effect -->
+<script src='/js/main/particles.min.js'></script>
+<script src="/js/main/index.js"></script>
+<!-- //scripts required for particle effect -->
+</body>
+
+</html>
