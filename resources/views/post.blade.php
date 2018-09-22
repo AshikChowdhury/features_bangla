19be72a272dda8c442962de5850778c348842e91
Ashik Chowdhury <ashikbracu@gmail.com>
2018-09-23 00:02:36 +0600

    like integration

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -13,15 +13,6 @@
     <div class="site-main-container">
         <!-- Start latest-post Area -->
         <section class="latest-post-area pb-120">
-            <div id="fb-root"></div>
-            <script>(function(d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
-                    fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
-            </script>
             <div class="container no-padding">
                 <div class="row">
                     <div class="col-lg-8 post-list">
